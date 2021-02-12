77944aae7cca2e36dd64d0a2c41f6d464f065cc1
JacobLoe <loebkens@uni-potsdam.de>
2021-02-12 15:12:40 +0100

    renamed docker image

--- tf_gpu_test.py
+++ tf_gpu_test.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+#print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
 
 if tf.test.gpu_device_name():
 
