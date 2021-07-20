a216be3a0e359f58f4c1f1a8126dbc4b69459eb7
JacobLoe <Jcbl423@gmail.com>
2021-07-20 14:42:12 +0200

    changed action answer to select_room

--- MapWorld/MapGame.py
+++ MapWorld/MapGame.py
@@ -17,7 +17,6 @@ class MapWorldGym(gym.Env):
                  caption_checkpoints="./im2txt/checkpoints/im2txt_5M/model.ckpt-5000000",
                  caption_vocab='./im2txt/vocab/word_counts.txt',
                  image_resolution=(480, 854)):
-        # TODO possibly kick out useless variables from init
         # the dimensions of the map
         self.n = n
         self.m = m
@@ -27,8 +26,7 @@ class MapWorldGym(gym.Env):
         self.room_types = room_types
         self.room_repetitions = room_repetitions
 
-        self.image_caption_model = Captioning(caption_checkpoints,
-                                              caption_vocab)
+        self.image_caption_model = Captioning(caption_checkpoints, caption_vocab)
 
         #
         self.current_room = []
@@ -40,14 +38,17 @@ class MapWorldGym(gym.Env):
 
         # TODO represent observation space and action space as gym.spaces
         # TODO like in https://github.com/openai/gym/blob/master/gym/envs/classic_control/cartpole.py
-        self.total_available_actions = {0: 'north', 1: 'east', 2: 'south', 3: 'west', 4: 'answer'}
+        self.total_available_actions = {0: 'north', 1: 'east', 2: 'south', 3: 'west', 4: 'select_room'}
 
         # the state consists of: current room as numpy ndarray of shape (, , 3),
         # target room question as string,
         # available actions as list of string, max len=5
         self.state = []
 
+        # done is False as long as an episode is running
+        # when an episode terminates room_found is turned to 1 when the correct was selected
         self.done = False
+        self.room_found = 0
 
         self.ade_path = ade_path
 
@@ -73,53 +74,61 @@ class MapWorldGym(gym.Env):
         # generate question based on the sampled map
         self.question, self.target_room = self.generate_question_from_image(self.mw.target_room)
 
-        # TODO rescale images to consistent resolution
         self.current_room_name = path.relpath(initial_state[0], self.ade_path)
         self.current_room = self.load_image(initial_state[0], self.image_resolution)
 
-        self.directions = initial_state[1] + ', answer.'
-        self.available_actions = self.directions[12:].split()
+        # directions contains the available actions in the current room as a string (returned as part of the state)
+        # 'select_room is always added at the end
+        # available_actions contains them as a list for internal use of the environment
+        self.directions = initial_state[1] + f', {self.total_available_actions[4]}.'
+        self.available_actions = self.directions[12:].split() # prepending fluff is removed, actions are seperated by ","
 
         # keep track of the total steps taken and the return
         self.model_return = 0
         self.model_steps = 0
 
-        # TODO check whether the outputs are numpy arrays (or strings)
         # return the initial state
         self.state = [self.current_room, self.question, self.directions]
         return self.state
 
-    def step(self, action):
+    def step(self, action_index):
         """
         Take one step in the environment
-        :param action: string, one of: north, east, south, west, answer
+        :param action_index: int, index corresponds to one of the actions in self.total_available_actions
         :return: list, contains the state, reward and signal if the game is done
         """
-        room_found = 0
-        # map action chosen by agent (integer) to action interpretable by MapWorld
-        action = self.total_available_actions[action]
-        if action == 'answer':
-            # TODO increase reward for a wrong guess compared to correct guess to encourage
-            if self.current_room_name == self.target_room:
-                reward = 1000.0
-                room_found = 1
-            else:
-                reward = -1000.0
-            # Terminate the game
-            self.done = True
-            self.state = [self.current_room, self.question, self.directions]
+        # map the chosen action index to the action string
+        action = self.total_available_actions[action_index]
+        if action == self.total_available_actions[4]:
+            reward = self.select_room()
         else:
             reward = self.move(action)
 
         self.model_return += reward
         self.model_steps += 1
 
-        return self.state, reward, self.done, room_found
+        return self.state, reward, self.done, self.room_found
+
+    def select_room(self):
+        """
+        Select_room ends the episode
+        Returns: a float, the reward for the action
+        """
+        if self.current_room_name == self.target_room:
+            reward = 1000.0
+            self.room_found = 1
+        else:
+            reward = -1000.0
+        # Terminate the game
+        self.done = True
+        self.state = [self.current_room, self.question, self.directions]
+
+        return reward
 
     def move(self, action):
         """
-        Moves in the map according to the action. If the action is not in the available actions stays in the current
-        room and penalizes heavily.
+        Moves in the map according to the action. If the action is not in the available actions,
+        the agent stays in the current room and the current state is returned again.
         Args:
             action: string, one from north, south, west, east
 
@@ -131,7 +140,7 @@ class MapWorldGym(gym.Env):
             state = self.mw.upd(action)
             self.current_room_name = path.relpath(state[0], self.ade_path)
             self.current_room = np.array(cv2.imread(state[0]))
-            self.directions = state[1] + ', answer'
+            self.directions = state[1] + f', {self.total_available_actions[4]}.'
         else:
             # TODO not sure what the correct reward here would be for taking an unavailable action
             # TODO maybe stop penalizing wrong actions
@@ -157,10 +166,10 @@ class MapWorldGym(gym.Env):
 
     def load_image(self, image_path, image_resolution):
         """
-        Loads an image from disk and reshapes while keeping the aspect ration intact.
+        Loads an image from disk and reshapes it to a given resolution.
         Args:
-            image_path: strind
-            image_width: integer
+            image_path: string
+            image_resolution: tuple
 
         Returns: Numpy array, reshaped image, height, width, channels,
         """
