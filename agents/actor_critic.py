23b4d954d78b55601d39ccba7f65d9dbb8711d96
JacobLoe <Jcbl423@gmail.com>
2021-08-05 17:27:53 +0200

    merged action and value network into one

--- agents/actor_critic.py
+++ agents/actor_critic.py
@@ -38,16 +38,11 @@ def actor_critic(mwg, model_parameters, training_parameters, base_path, logger, 
                                 output_size,
                                 num_layers)).to(device)
 
-    value_model = DataParallel(ValueModel(emsize,
-                               max_sequence_length,
-                               1,
-                               num_layers)).to(device)
-
     buffer = ReplayBuffer(training_parameters['replay_size'])
     # Named tuple for storing experience steps gathered during training
     Experience = namedtuple('Experience', field_names=['state', 'action', 'reward', 'done', 'new_state'])
 
-    print(action_model.device_ids, value_model.device_ids)
+    print(action_model.device_ids)
     # print(daeada)
     lr = training_parameters['learning_rate']
     num_episodes = training_parameters['num_episodes']
@@ -58,7 +53,6 @@ def actor_critic(mwg, model_parameters, training_parameters, base_path, logger, 
     starting_episode = 0
 
     action_optimizer = torch.optim.Adam(action_model.parameters(), lr=lr)
-    value_optimizer = torch.optim.Adam(action_model.parameters(), lr=lr)
 
     em_model = SentenceTransformer(model_parameters['embedding_model'])
 
@@ -93,11 +87,9 @@ def actor_critic(mwg, model_parameters, training_parameters, base_path, logger, 
             embeddings = em_model.encode(text)
             embedded_text_tensor = torch.FloatTensor([embeddings])
 
-            action_probabilities = action_model(im_tensor.to(device),
-                                                embedded_text_tensor.to(device))
+            action_probabilities, state_value = action_model(im_tensor.to(device),
+                                                             embedded_text_tensor.to(device))
             logger.debug('action_probabilities', action_probabilities)
-            state_value = value_model(im_tensor.to(device),
-                                      embedded_text_tensor.to(device))
             # action_probabilities = action_probabilities.cpu().detach().numpy()[0]
 
             # create a categorical distribution over the list of probabilities of actions
@@ -169,16 +161,12 @@ def actor_critic(mwg, model_parameters, training_parameters, base_path, logger, 
 
                     # reset gradients
                     action_optimizer.zero_grad()
-                    value_optimizer.zero_grad()
                     # sum up all the values of policy_losses and value_losses
                     action_loss = torch.stack(policy_losses).sum()
-                    value_loss = torch.stack(value_losses).sum()
 
                     # perform backprop
                     action_loss.backward()
-                    value_loss.backward()
                     action_optimizer.step()
-                    value_optimizer.step()
 
                     batch_rewards = []
                     batch_actions = []
@@ -205,15 +193,19 @@ class ActionModel(nn.Module):
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
         self.fc_cnn1 = nn.Linear(121104, 1200)   # layer size is result of image res (360x360) after conv + pool
-
         self.fc_cnn2 = nn.Linear(1200, max_sequence_length)
 
         # text processing
         self.lstm1 = nn.LSTM(1, emsize, batch_first=True, num_layers=num_layers)
         self.fc_lstm = nn.Linear(emsize, max_sequence_length)
 
-        self.fc4 = nn.Linear(max_sequence_length, max_sequence_length)
-        self.fc5 = nn.Linear(max_sequence_length, output_size)
+        # action model
+        self.fc4_action = nn.Linear(max_sequence_length, max_sequence_length)
+        self.fc5_action = nn.Linear(max_sequence_length, output_size)
+
+        # value model
+        self.fc4_value = nn.Linear(max_sequence_length, max_sequence_length)
+        self.fc5_value = nn.Linear(max_sequence_length, 1)
 
         # self.init_weights()
 
@@ -229,62 +221,19 @@ class ActionModel(nn.Module):
         text = torch.tanh(self.fc_lstm(text))
         text = torch.mean(text, dim=1)
 
+        # combine image and text into one vector
         output = torch.mul(cnn, text)
-        output = F.relu(self.fc4(output))
-        output = self.fc5(output)
-        actions = F.softmax(output, dim=1)
-        return actions
 
-    def init_weights(self):
-        # TODO add init for cnn
-        # make initrange a parameter
-        initrange = 0.1
-        # self.encoder.weight.data.uniform_(-initrange, initrange)
-        # self.decoder.bias.data.zero_()
-        # self.decoder.weight.data.uniform_(-initrange, initrange)
+        # compute the best action for a state
+        actions = F.relu(self.fc4_action(output))
+        actions = self.fc5_action(actions)
+        actions = F.softmax(actions, dim=1)
 
+        # compute the value of being in a state
+        value = F.relu(self.fc4_value(output))
+        value = self.fc5_value(value)
 
-class ValueModel(nn.Module):
-    def __init__(self, emsize, max_sequence_length, output_size, num_layers):
-        super(ValueModel, self).__init__()
-        # TODO with the sentence transformer max_sequence_length is not a thing anymore
-        # TODO maybe replace it with something else (emsize, etc)
-        # TODO https://arxiv.org/pdf/1902.07742.pdf
-
-        # TODO look into padding of input image
-        # CNN
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc_cnn1 = nn.Linear(121104, 1200)   # layer size is result of image res (360x360) after conv + pool
-
-        self.fc_cnn2 = nn.Linear(1200, max_sequence_length)
-
-        # text processing
-        self.lstm1 = nn.LSTM(1, emsize, batch_first=True, num_layers=num_layers)
-        self.fc_lstm = nn.Linear(emsize, max_sequence_length)
-
-        self.fc4 = nn.Linear(max_sequence_length, max_sequence_length)
-        self.fc5 = nn.Linear(max_sequence_length, output_size)
-
-        # self.init_weights()
-
-    def forward(self, im, text):
-        cnn = self.pool(F.relu(self.conv1(im)))
-        cnn = self.pool(F.relu(self.conv2(cnn)))
-        cnn = torch.flatten(cnn, 1)     # flatten all dimensions except batch
-        cnn = F.relu(self.fc_cnn1(cnn))
-        cnn = torch.tanh(self.fc_cnn2(cnn))
-
-        text, _ = self.lstm1(text.unsqueeze(-1))
-
-        text = torch.tanh(self.fc_lstm(text))
-        text = torch.mean(text, dim=1)
-
-        output = torch.mul(cnn, text)
-        output = F.relu(self.fc4(output))
-        output = self.fc5(output)
-        return output
+        return actions, value
 
     def init_weights(self):
         # TODO add init for cnn
