f22ce0efa07c12018066bc9bf597be36b4ed3256
JacobLoe <Jcbl423@gmail.com>
2021-09-28 22:30:29 +0200

    fixed error in split_captions

--- localized_narratives/split_captions.py
+++ localized_narratives/split_captions.py
@@ -1,8 +1,10 @@
 from tqdm import tqdm
+import numpy as np
 import argparse
 import json
 import glob
 import os
+import jsonlines
 
 if __name__ == '__main__':
 
@@ -16,12 +18,50 @@ if __name__ == '__main__':
     p = os.path.join(args.in_path, '*.txt')
     captions_path_list = glob.glob(p)
 
+    localized_narratives = {}
+    with jsonlines.open('ade20k_train_captions.jsonl', mode='r') as f:
+        for line in f:
+            image_name = line['image_id']
+            caption = line['caption']
+            localized_narratives[image_name] = caption
+
+    print('localized narratives')
+    c = [len(localized_narratives[k].split()) for k in localized_narratives]
+    print(f'Min caption length {np.min(c)}')
+    print(f'Max caption length {np.max(c)}')
+    print(f'Mean caption length {np.mean(c)}')
+
+    # TODO fix missing captions and wrong captions
     split_captions = {}
-    for caption in tqdm(captions_path_list):
-        with open(caption, 'r') as f:
+    for caption_path in tqdm(captions_path_list):
+        with open(caption_path, 'r') as f:
             lines = [s.strip('\n') for s in f.readlines()]
-            image_name = os.path.split(caption)[1].strip('.txt')
-            split_captions[image_name] = lines
+            image_name = os.path.split(caption_path)[1].strip('.txt')
+            if len(lines)!=0:
+                if len(lines[0].split()) == 236:
+                    split_captions[image_name] = ['One person standing']
+                    print(lines[0])
+                else:
+                    split_captions[image_name] = lines
+            else:
+                split_captions[image_name] = localized_narratives[image_name]
+
+    # i = 0
+    # for k in tqdm(split_captions):
+    #     try:
+    #         print(split_captions[k][0])
+    #     except:
+    #         # print(split_captions[k])
+    #         # print(localized_narratives[k])
+    #         # break
+    #         i+=1
+    # print(i)
+
+    print('split captions')
+    c = [len(split_captions[k][0].split()) for k in split_captions]
+    print(f'Min caption length {np.min(c)}')
+    print(f'Max caption length {np.max(c)}')
+    print(f'Mean caption length {np.mean(c)}')
 
     with open(args.out_path, 'w') as f:
         json.dump(split_captions, f, sort_keys=True)
