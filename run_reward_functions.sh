3e7962f4ee8c80e73fcee8fa8f9b4b1fc141dc20
JacobLoe <Jcbl423@gmail.com>
2022-01-23 21:31:24 +0100

    refactored reward function names

--- run_reward_functions.sh
+++ run_reward_functions.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
-#python main.py random --parameters parameters/reward_function_r1.json --base_path results/random/2022-01-12_r1
-#echo " "
-python main.py random --parameters parameters/reward_function_r3.json --base_path results/random/2022-01-14_r3
+python main.py random --parameters parameters/reward_function_r5.json --base_path results/random/2022-01-23_r5
 echo " "
-python main.py random --parameters parameters/reward_function_r5.json --base_path results/random/2022-01-16_r5
+python main.py random --parameters parameters/reward_function_r6.json --base_path results/random/2022-01-25_r6
 echo " "
-python main.py random --parameters parameters/reward_function_r6.json --base_path results/random/2022-01-18_r6
+python main.py random --parameters parameters/reward_function_r7.json --base_path results/random/2022-01-26_r7
 echo " "
-python main.py random --parameters parameters/reward_function_r7.json --base_path results/random/2022-01-20_r7
+python main.py ac --parameters parameters/reward_function_r5.json --base_path results/actor_critic/2022-01-27_r5
 echo " "
-python main.py random --parameters parameters/reward_function_r8.json --base_path results/random/2022-01-22_r8
+python main.py ac --parameters parameters/reward_function_r5_masked.json --base_path results/actor_critic/2022-01-28_r5_masked
 echo " "
-python main.py random --parameters parameters/reward_function_r3_masked.json --base_path results/random/2022-01-24_r3_masked
+python main.py ac --parameters parameters/reward_function_r6.json --base_path results/actor_critic/2022-01-29_r6
 echo " "
-python main.py random --parameters parameters/reward_function_r5_masked.json --base_path results/random/2022-01-26_r5_masked
+python main.py ac --parameters parameters/reward_function_r7_masked.json --base_path results/actor_critic/2022-01-30_r7_masked
 echo " "
-python main.py random --parameters parameters/reward_function_r6_masked.json --base_path results/random/2022-01-28_r6_masked
-echo " "
-python main.py random --parameters parameters/reward_function_r8_masked.json --base_path results/random/2022-01-30_r8_masked
