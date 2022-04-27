be4962f84210bbb626007c7b117353b9bf9a5fab
JacobLoe <Jcbl423@gmail.com>
2022-04-27 21:56:24 +0200

    updated run reward functions script

--- run_reward_functions.sh
+++ run_reward_functions.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
-python main.py random --parameters parameters/reward_function_r5.json --base_path results/random/2022-01-23_r5
+python main.py ac --parameters parameters/reward_function_r1_masked.json --base_path results/actor_critic/2022-04-27_r1_masked_1
 echo " "
-python main.py random --parameters parameters/reward_function_r6.json --base_path results/random/2022-01-25_r6
+python main.py ac --parameters parameters/reward_function_r1_masked.json --base_path results/actor_critic/2022-04-29_r1_masked_2
 echo " "
-python main.py random --parameters parameters/reward_function_r7.json --base_path results/random/2022-01-26_r7
-echo " "
-python main.py ac --parameters parameters/reward_function_r5.json --base_path results/actor_critic/2022-01-27_r5
-echo " "
-python main.py ac --parameters parameters/reward_function_r5_masked.json --base_path results/actor_critic/2022-01-28_r5_masked
-echo " "
-python main.py ac --parameters parameters/reward_function_r6.json --base_path results/actor_critic/2022-01-29_r6
-echo " "
-python main.py ac --parameters parameters/reward_function_r7_masked.json --base_path results/actor_critic/2022-01-30_r7_masked
+python main.py ac --parameters parameters/reward_function_r1_masked.json --base_path results/actor_critic/2022-04-30_r1_masked_3
 echo " "
